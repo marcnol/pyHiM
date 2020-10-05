cb49b99fefa7a4d293216367b288322000a6f46d
marcnol <marcnol@gmail.com>
2020-10-05 12:55:38 +0200

    Prototyping and testing of improvements to image alignment

--- tests/testAlignCV2.py
+++ tests/testAlignCV2.py
@@ -4,15 +4,35 @@
 Created on Sat Sep 26 16:00:31 2020
 
 @author: marcnol
+
+This series of scripts was written to test and prototype:
+    
+    - alignment of masks using CV2
+    - [Not ported] testing translations and afine transformations
+    - [Not ported] testing multi-scale translational alignment by zooming out of the center of the image
+    - [ported] Displaying image difference to assess alignment
+    - [ported] Test block translational alignment
+
 """
 import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
 from datetime import datetime
-
+from skimage.util.shape import view_as_blocks
+from numpy import linalg as LA
+from tqdm import trange
+from skimage import data, segmentation,measure
+from skimage import filters
+from skimage.feature import register_translation
+from scipy.ndimage import shift as shiftImage
+import cv2
+from skimage.exposure import match_histograms
 
 ## Functions
+    
+
+
 
 def alignCV2(im1,im2,warp_mode):
     
@@ -52,7 +72,90 @@ def applyCorrection(im2,warp_matrix):
 
     return im2_aligned 
 
-#%%
+
+def showImages(image1,image2,im2_aligned,fileName):
+    
+    fig, (ax1,ax2) = plt.subplots(1,2)
+    fig.set_size_inches((60, 30))
+    cmap = 'seismic'    
+    ax1.imshow(image1-image2, cmap=cmap)
+    ax1.axis("off")
+
+    ax2.imshow(image1-im2_aligned, cmap=cmap)
+    ax2.axis("off")
+    
+    plt.savefig(fileName)
+
+
+def alignImagesByBlocks(I1,I2,blockSize,upsample_factor=100):
+
+    Block1=view_as_blocks(image1,blockSize)
+    Block2=view_as_blocks(image2,blockSize)
+    
+    shiftImageNorm= np.zeros((Block1.shape[0],Block1.shape[1]))
+    shiftedImage = np.zeros((Block1.shape[0],Block1.shape[1],2))
+    rmsImage= np.zeros((Block1.shape[0],Block1.shape[1]))
+    
+    for i in trange(Block1.shape[0]):
+        for j in range(Block1.shape[1]):
+            
+            # shift, error, diffphase = register_translation(Block1[i,j], Block2[i,j],upsample_factor=upsample_factor)
+            # shiftImageNorm[i,j] = LA.norm(shift)
+            # shiftedImage[i,j,0], shiftedImage[i,j,1] = shift[0], shift[1]
+            # I2_aligned = shiftImage(I2, shift)
+              
+            cc, warp_matrix = alignCV2(Block1[i,j], Block2[i,j], warp_mode)
+            shiftImageNorm[i,j] = LA.norm(warp_matrix[:,2])
+            shiftedImage[i,j,0],shiftedImage[i,j,1] = warp_matrix[:,2][0], warp_matrix[:,2][1]
+            I2_aligned = applyCorrection(I2,warp_matrix)
+            
+            rmsImage[i,j] =np.sum(np.sum(np.abs(I1-I2_aligned),axis=1)) 
+            
+    # calculates optimal shifts by polling blocks showing the best RMS
+    tolerance=0.1
+    
+    # threshold = filters.threshold_otsu(rmsImage)
+    threshold = (1+tolerance)*np.min(rmsImage)
+    mask = rmsImage < threshold 
+    contours = measure.find_contours(rmsImage, threshold)
+    contour = sorted(contours, key=lambda x: len(x))[-1]
+
+    meanShifts =  [np.mean(shiftedImage[mask,0]), np.mean(shiftedImage[mask,1])]
+    warp_matrix[:,2]=meanShifts
+    stdShifts =[np.std(shiftedImage[mask,0]), np.std(shiftedImage[mask,1])]
+    meanShiftNorm = np.mean(shiftImageNorm[mask])
+    
+    meanError = np.mean(rmsImage[mask])
+
+    relativeShifts= shiftImageNorm-meanShiftNorm
+    
+    print("Mean polled XY shifts: {:.2f}({:.2f}) px | {:.2f}({:.2f}) px".format(meanShifts[0], stdShifts[0], meanShifts[1], stdShifts[1]))
+    
+    return meanShifts, warp_matrix, meanError, mask, relativeShifts, rmsImage, contour
+
+def plottingBlockALignmentResults(relativeShifts, rmsImage, contour, fileName='BlockALignmentResults.png'):    
+
+    # plotting
+    fig, axes = plt.subplots(1,2)
+    ax=axes.ravel()
+    fig.set_size_inches((10,5))
+    
+    cbwindow=5
+    p1 = ax[0].imshow(relativeShifts,cmap='RdBu',vmin=-cbwindow, vmax=cbwindow)
+    ax[0].plot(contour.T[1], contour.T[0], linewidth=2, c='k')
+    ax[0].set_title("relative shift, px")
+    fig.colorbar(p1,ax=ax[0],fraction=0.046, pad=0.04)
+    
+    p2 = ax[1].imshow(rmsImage,cmap='terrain',vmin=np.min(rmsImage), vmax=np.max(rmsImage))
+    ax[1].plot(contour.T[1], contour.T[0], linewidth=2, c='k')
+    ax[1].set_title("RMS")
+    fig.colorbar(p2,ax=ax[1],fraction=0.046, pad=0.04)
+    
+    for x in range(len(ax)):
+        ax[x].axis('off')
+
+    
+#%% loading and preprocessing of example images
     
 ## main
 rootFolder = "/home/marcnol/data/Embryo_debug_dataset/Experiment_18/zProject"
@@ -62,13 +165,17 @@ imFileName2=rootFolder+os.sep+"scan_001_RT37_001_ROI_converted_decon_ch00_2d.npy
 image1 = np.load(imFileName1).squeeze()
 image2 = np.load(imFileName2).squeeze()
 
-image1 = image1 / image1.max()
-image2 = image2 / image2.max()
+images=(image1,image2)
 
-image1 = np.float32(image1)
-image2 = np.float32(image2)
+image1,image2=list(map(lambda x:np.float32(x/x.max()), images))
 
-#%%
+# image1 = image1 / image1.max()
+# image2 = image2 / image2.max()
+
+# image1 = np.float32(image1)
+# image2 = np.float32(image2)
+
+#%% Testing translational and afine transformations with CV2
 
 # Define the motion model
 # model='afine'
@@ -93,8 +200,8 @@ print('Done registering!')
 
 print("Elapsed time: {}".format(datetime.now() - begin_time))
 
-#%%
 
+# showing RGB image
 sz = image1.shape
 
 fig, (ax1,ax2) = plt.subplots(1,2)
@@ -113,23 +220,65 @@ ax2.axis("off")
 plt.savefig(outputFileName)
 
 
-#%% difference image
+#%% Testing difference image
 
-outputFileName = '/home/marcnol/Downloads/'+'difference_'+model+'.png'
-cmap = 'seismic'
+showImages(image1,image2, im2_aligned,'/home/marcnol/Downloads/'+'difference_'+model+'.png')
 
-fig, (ax1,ax2) = plt.subplots(1,2)
-fig.set_size_inches((60, 30))
+#%% test multiscale alignments
 
-nullImage = np.zeros(sz)
+imageSize=image1.shape[0]
+c=int(np.round(imageSize/2))
 
-ax1.imshow(image1-image2, cmap=cmap)
-ax1.axis("off")
+factors=[16,8,4,2,1]
+windows=[int(imageSize/x) for x in factors]
+shifts = list()
+rms=list()
+CC=list()
 
-ax2.imshow(image1-im2_aligned, cmap=cmap)
-ax2.axis("off")
+for window in windows:
+    if window==imageSize:
+        I1=image1
+        I2=image2
+    else:
+        I1 = image1[c-window:c+window, c-window:c+window]
+        I2 = image2[c-window:c+window, c-window:c+window]
+ 
+    cc, warp_matrix = alignCV2(I1, I2, warp_mode)
 
-plt.savefig(outputFileName)
+    I2_aligned = applyCorrection(I2,warp_matrix)
+
+    shifts.append(warp_matrix[:,2]) 
+    rms.append(np.sum(np.sum(np.abs(I1-I2_aligned),axis=1)))
+    CC.append(cc)
+    
+    print('Aligned images for window: {}'.format(window))
+    fileName='/home/marcnol/Downloads/'+'difference_'+str(window)+'.png'
+    
+    showImages(I1,I2,I2_aligned, fileName)
+    
+
+# display
+    
+fig, (ax1,ax2,ax3) = plt.subplots(1,3)
+fig.set_size_inches((15,5))
+
+ax1.plot(windows,CC,'o-')
+ax1.set_ylabel('cc')
+
+ax2.plot(windows,rms,'o-')
+ax2.set_ylabel('rms')
+
+ax3.plot(windows, [x[0] for x in shifts]-shifts[-1][0],'o-')
+ax3.plot(windows, [x[1] for x in shifts]-shifts[-1][1],'+-')
+ax3.set_ylabel('relative shifts')
 
 
+#%% test blocks alignments 
 
+# image2=match_histograms(image2,image1)
+upsample_factor=100
+blockSize=(256,256)
+       
+meanShifts, warp_matrix, meanError, mask, relativeShifts, rmsImage, contour  = alignImagesByBlocks(image1,image2,blockSize,upsample_factor)
+
+plottingBlockALignmentResults(relativeShifts, rmsImage, contour)
