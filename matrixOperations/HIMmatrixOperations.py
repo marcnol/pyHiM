bug detected in Rg calculation. Not clear yet how to solve.

matrixOperations/HIMmatrixOperations.py
@@ -1530,9 +1530,12 @@ def getRgFromPWD(PWDmatrix, minFracNotNaN=0.8):
     sq = np.square(PWDmatrix)
     sq = np.nansum(sq) # default is to compute the sum of the flattened array
     
-    Rg_sq = sq / (2 * (2*numNotNan + PWDmatrix.shape[0])) # replaces 1/(2*N^2)
+    # Rg_sq = sq / (2 * (2*numNotNan + PWDmatrix.shape[0])) # replaces 1/(2*N^2)
     
-    Rg_sq = Rg_sq / 2 # there is a factor of two because interactions pairwise distances counted twice.
+    ## Rg_sq = Rg_sq / 2 # there is a factor of two because interactions pairwise distances counted twice.
+
+    Rg_sq = sq / (8 * numNotNan**2) # replaces 1/(2*N^2)
+    # Rg_sq = sq / (2 * (PWDmatrix.shape[0])**2) # replaces 1/(2*N^2)
     
     Rg = np.sqrt(Rg_sq)
     

plots/figureSingleCell.py
@@ -29,7 +29,7 @@ from matrixOperations.HIMmatrixOperations import analysisHiMmatrix
 
 font = {'family' : 'DejaVu Sans',
         'weight' : 'normal',
-        'size'   : 22}
+        'size'   : 25}
 
 matplotlib.rc('font', **font)
 
@@ -522,7 +522,7 @@ def plotsRgvalues(HiMdata,nRows,runParameters,
     # KDE fit
     # fist finds best bandwidth
     print("Calculating optimal KDE bandwidth...")
-    bandwidths = 10 ** np.linspace(-1, 0, 20)
+    bandwidths = 10 ** np.linspace(-2, 0, 20)
     grid = GridSearchCV(KernelDensity(kernel='gaussian'),
                         {'bandwidth': bandwidths},
                         cv=LeaveOneOut())