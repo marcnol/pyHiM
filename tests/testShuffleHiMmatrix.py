a9a7e846eb7c1288143729c3264484912151e8ce
marcnol <marcnol@gmail.com>
2021-04-18 20:48:50 +0200

    + test for shuffle  HiM matrix

---
+++ tests/testShuffleHiMmatrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 20:48:16 2021
+
+@author: marcnol
+"""
+
+from HIMmatrixOperations import shuffleMatrix
+
+shuffle = [0, 7, 1, 8, 2, 9, 10, 17, 11, 18, 12, 19, 13, 3, 14, 4, 15, 5, 16, 6]
+
+matrix_2D_shuffled = shuffleMatrix(matrix_2D, shuffle)
