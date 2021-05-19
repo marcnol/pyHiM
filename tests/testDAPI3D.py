60b8855cfdd1be670c9d78c7d529117ef3dc1c61
marcnol <marcnol@gmail.com>
2021-05-19 13:22:44 +0200

    remove unnecessary imports

--- tests/testDAPI3D.py
+++ tests/testDAPI3D.py
@@ -12,15 +12,13 @@ import os
 from mayavi.mlab import contour3d
 from matplotlib.pylab import plt
 
-from imageProcessing.imageProcessing import Image
-from fileProcessing.fileManagement import Parameters, log
+# from imageProcessing.imageProcessing import Image
+# from fileProcessing.fileManagement import Parameters, log
 from skimage import io
 
 #%% loads data
 
 rootFolder = "/home/marcnol/Downloads"
-
-fileName = "scan_006_DAPI_001_ROI_converted_decon_ch00.tif"
 
 rootFolder = "/home/marcnol/grey/rawData_2020/Exp_Combinatory_3_Tof/ROIs/ROI001"
 fileName = "scan_007_DAPI_001_ROI_converted_decon_ch00.tif"
@@ -30,27 +28,37 @@ rootFolder = "/home/marcnol/data/Embryo_debug_dataset/test_dataset"
 fileName = "scan_001_RT27_001_ROI_converted_decon_ch01.tif"
 fileNameF="scan_006_DAPI_001_ROI_converted_decon_ch00.tif"
 
+rootFolder = "/home/marcnol/data/Embryo_debug_dataset/test_dataset"
+fileName = "scan_001_RT27_001_ROI_converted_decon_ch01.tif"
+fileNameF="scan_006_DAPI_001_ROI_converted_decon_ch00.tif"
+
+rootFolder = "/home/marcnol/grey/users/marcnol/test_HiM/run_zBinning2"
+fileName = "scan_001_RT41_003_ROI_converted_decon_ch01.tif"
+fileNameF = "scan_006_DAPI_003_ROI_converted_decon_ch00.tif"
+
 fullFileName = rootFolder + os.sep + fileName
 fullFileNameF = rootFolder + os.sep + fileNameF
 
 data = io.imread(fullFileName).squeeze()
 dataF = io.imread(fullFileNameF).squeeze()
 
-subdata = data[20:58, 0:2000, 0:2000]
-subdataF = dataF[12:40, 0:2000, 0:2000]
-
 
 #%% Displays overlays between two channels
 
-
-# fig, ax=plt.subplots()
-# ax.imshow(data[:,500,:],alpha=0.7,cmap='Reds')
-# ax.imshow(dataF[:,500,:]*1, alpha=.7, cmap='Blues')
+subdata = data[10:50, 0:2000, 0:2000]
+subdataF = dataF[10:50, 0:2000, 0:2000]
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.imshow(data[:, :, 500]*100)
-ax2.imshow(dataF[:, :, 500] * 10)
+ax1.imshow(np.sum(data[:,450:550,:]*10,axis=1),alpha=0.7,cmap='Reds')
+ax1.imshow(np.sum(dataF[:,450:550,:]*1,axis=1), alpha=.7, cmap='Blues')
 
+ax2.imshow(np.sum(data[:,:,:]*10,axis=0),alpha=0.7,cmap='Reds')
+ax2.imshow(np.sum(dataF[:,:,:]*1,axis=0), alpha=.7, cmap='Blues')
+
+# fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+# ax1.imshow(data[:, :, 500]*100)
+# ax2.imshow(dataF[:, :, 500] * 10)
+# ax3.imshow(dataF[35,:,:])
 
 #%% Displays XY and YZ projections of the 3D volume
 
