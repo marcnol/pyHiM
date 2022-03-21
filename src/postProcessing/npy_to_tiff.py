abf906410d1d30d685bb99430aef5189c0b2fbc5
marcnol <marcnol@gmail.com>
2022-03-21 21:29:26 +0100

    minor formatting fix

--- src/postProcessing/npy_to_tiff.py
+++ src/postProcessing/npy_to_tiff.py
@@ -9,7 +9,6 @@ converts NPY to TIFF
 
 """
 
-# from apifish.stack.io import read_image as read_image
 import os
 import glob
 import sys
@@ -21,43 +20,37 @@ from tifffile import TiffWriter
 files = sys.argv[1:]
 
 if select.select([sys.stdin, ], [], [], 0.0)[0]:
-    print("Have data!")
+    print("Found data in stdin.\n!")
 
-    piped_files = list()    
-    for line in sys.stdin:
-        piped_files.append(line.rstrip("\n"))    
-
-    if len(piped_files):
+    piped_files = [line.rstrip("\n") for line in sys.stdin]
+    if len(piped_files)>0:
         files = piped_files
         print(f"Read {piped_files} files list from stdin!\n")
 else:
     print("No stdin")
-  
-  
-    
-    
+
 if len(files)>0:
-    
+
     print(f"{len(files)} files to process: ")
     for file in files:
         print(f"\n{os.path.basename(file)}")
-        
+
         data = np.load(file)
         # data.astype('float')
-        
+
         file_out = file.rsplit('.')[0]+'.tif'
         print(f"output file: {file_out}")
-        
+
         with TiffWriter(file_out) as tif:
-            tif.save(data.astype(np.uint16))        
-        
+            tif.save(data.astype(np.uint16))
+
         print(f"Finished processing {len(files)} files")
-        
+
 else:
     print("Please provide list of NPY files to process! none provided")
-    
+
     print("\n Usage:\n")
-    print("with find:")    
+    print("with find:")
     print("npy_to_tiff $(find -name \"*ch0*_2d_registered.npy\")\n\n")
     print("with pipes:")
-    print("ls *ch0*_2d_registered.npy | npy_to_tiff")        
+    print("ls *ch0*_2d_registered.npy | npy_to_tiff")
