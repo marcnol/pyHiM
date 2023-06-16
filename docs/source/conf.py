a67d1edaddd8b412b0ffc8ec7e9f97362949f4fe
Xavier DEVOS <xavierdevos@outlook.fr>
2023-06-16 10:46:09 +0200

    Fix - Force light mode for the online doc * Fix matplotlib version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,12 +78,17 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 #
 html_theme = 'sphinx_book_theme'
 
+html_context = {
+   "default_mode": "light"
+}
+
 html_theme_options = {
     "repository_url": "https://github.com/marcnol/pyHiM",
     "use_repository_button": True,
     "use_edit_page_button": False,
     "path_to_docs": "docs",
-    "logo_only": True
+    "logo_only": True,
+    "navbar_end": ["navbar-icon-links"]
 }
 
 html_logo = "_static/logo_pyHiM.png"
