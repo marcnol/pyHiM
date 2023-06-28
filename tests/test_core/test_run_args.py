de2a588c066a71870c624596022f78a2f04ad515
Xavier Devos <xavierdevos@outlook.fr>
2023-06-28 16:27:39 +0200

    Test - Add tests for core/run_args.py

---
+++ tests/test_core/test_run_args.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tempfile
+
+import pytest
+
+from core import run_args as ra
+
+
+def test_check_consistency():
+    # Test SystemExit of data_path
+    with pytest.raises(SystemExit, match=r".*input/path/does/not/exist.*"):
+        run_args = ra.RunArgs(["-F", "input/path/does/not/exist"])
+        run_args._check_consistency()
+
+    # Test SystemExit of cmd_list
+    with pytest.raises(SystemExit, match=r"'ratafia' .*"):
+        run_args = ra.RunArgs(["-C", "ratafia"])
+        run_args._check_consistency()
+
+    # Test SystemExit of threads
+    with pytest.raises(SystemExit, match=r".*Number of threads.*"):
+        run_args = ra.RunArgs(["-T", "-1"])
+        run_args._check_consistency()
+
+    # Test SystemExit of stardist_basename
+    with pytest.raises(SystemExit, match=r".*in/path/stradist/basename.*"):
+        run_args = ra.RunArgs(["-S", "in/path/stradist/basename"])
+        run_args._check_consistency()
