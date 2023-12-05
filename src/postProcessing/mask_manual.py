d4da551ff92209d07eac8b43bdca2b4281c36177
Xavier DEVOS <xavierdevos@outlook.fr>
2023-12-05 20:17:37 +0100

    Shift mask_manual.py

--- src/postProcessing/mask_manual.py
+++ src/postProcessing/mask_manual.py
@@ -5,16 +5,106 @@ Created on Wed Oct 11 17:47:13 2023
 
 @author: marcnol
 """
-import sys
 import argparse
+import json
 import os
+import select
+import subprocess
+import sys
+
 import numpy as np
-
-from matplotlib import pyplot as plt
-from skimage import exposure, io
-from roipoly import MultiRoi
-
 from astropy.visualization import simple_norm
+from matplotlib import pyplot as plt
+from roipoly import MultiRoi
+from scipy.ndimage import shift as shift_image
+from skimage import exposure, io
+from tifffile import imread, imwrite
+
+
+def find_roi_name_in_path(mask_3d_path):
+    return os.path.basename(mask_3d_path).split("_")[3]
+
+
+def find_label_in_path(mask_3d_path):
+    return os.path.basename(mask_3d_path).split("_")[2]
+
+
+def load_json(file_name):
+    """Load a JSON file like a python dict
+
+    Parameters
+    ----------
+    file_name : str
+        JSON file name
+
+    Returns
+    -------
+    dict
+        Python dict
+    """
+    if os.path.exists(file_name):
+        with open(file_name, encoding="utf-8") as json_file:
+            return json.load(json_file)
+    print(f"[WARNING] path {file_name} doesn't exist!")
+    raise ValueError
+
+
+def _shift_xy_mask_3d(image, shift):
+    number_planes = image.shape[0]
+    print(f"> Shifting {number_planes} planes")
+    shift_3d = np.zeros((3))
+    shift_3d[0], shift_3d[1], shift_3d[2] = 0, shift[0], shift[1]
+    return shift_image(image, shift_3d)
+
+def load_params():
+    # loads dicShifts with shifts for all rois and all labels
+    if os.path.exists("parameters.json"):
+        print("Load parameters.json")
+        return load_json("parameters.json")
+    elif os.path.exists("infoList.json"):
+        print(
+            "[WARNING] 'infoList.json' is a DEPRECATED file name, please rename it 'parameters.json'"
+        )
+        print("Load infoList.json")
+        return load_json("infoList.json")
+    else:
+        raise ValueError("[ERROR] 'parameters.json' file not found.")
+
+def get_dict_shifts():
+    params = load_params()
+    dict_shifts_path = (
+        params["common"]["alignImages"].get("register_global_folder", "register_global")
+        + os.sep
+        + "data"
+        + os.sep
+        + params["common"]["alignImages"].get("outputFile", "shifts")
+        + ".json"
+    )
+    return load_json(dict_shifts_path)
+
+def shift_3d_mask(mask_3d_path):
+    roi_name = find_roi_name_in_path(mask_3d_path)
+    label = find_label_in_path(mask_3d_path)
+    dict_shifts = get_dict_shifts()
+    # uses existing shift calculated by align_images
+    try:
+        shift = dict_shifts[f"ROI:{roi_name}"][label]
+        print("> Applying existing XY shift...")
+    except KeyError as e:
+        shift = None
+        raise SystemExit(
+            f"# Could not find dictionary with alignment parameters for this ROI: ROI:{roi_name}, label: {label}"
+        ) from e
+
+    mask_3d = imread(mask_3d_path).squeeze()
+
+    # applies XY shift to 3D stack
+    print(f"$ Applies shift = [{shift[0]:.2f} ,{shift[1]:.2f}]")
+    mask_3d_registered = _shift_xy_mask_3d(mask_3d, shift)
+    mask_3d_registered_path = mask_3d_path.split(".")[0] + "_3d_registered.tif"
+    imwrite(mask_3d_registered_path, mask_3d_registered)
+    return mask_3d_registered_path
+
 
 def show_image(data_2d, normalization="simple", size=(10, 10)):
     fig = plt.figure()
@@ -108,7 +198,11 @@ def main():
         
     print(f"parameters> input_file: {input_file}\n")
 
-    creates_user_mask(input_file,label)
+
+    file_registered = shift_3d_mask(input_file)
+    creates_user_mask(file_registered,label)
+    # Delete tempo registered file
+    os.unlink(file_registered)
     
     print("="*9+"Finished execution"+"="*9)
 
