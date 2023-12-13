fa1731b206dcdbd29a638950c51f98b71286ea1c
Xavier Devos <xavierdevos@outlook.fr>
2023-12-13 19:42:45 +0100

    Tuto - Improve traces display

--- docs/source/getting_started/tutorials/notebooks/Jupyter_functions_plots.py
+++ docs/source/getting_started/tutorials/notebooks/Jupyter_functions_plots.py
@@ -22,9 +22,22 @@ from tifffile import TiffWriter
 def copy_localization_table(Input_folder):
     source = os.path.join(Input_folder, "localizations_3D_barcode_example.dat")
     dest = os.path.join(
-        Input_folder, "localize_3d", "data", "localizations_3D_barcode.dat"
+        Input_folder,
+        "analysis_localizations",
+        "localize_3d",
+        "data",
+        "localizations_3D_barcode.dat",
     )
-    os.makedirs(os.path.join(Input_folder, "localize_3d", "data"))
+    folder_path = os.path.join(
+        Input_folder, "analysis_localizations", "localize_3d", "data"
+    )
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    shutil.copyfile(source, dest)
+
+    # copy the parameters.json in new root
+    source = os.path.join(Input_folder, "parameters.json")
+    dest = os.path.join(Input_folder, "analysis_localizations", "parameters.json")
     shutil.copyfile(source, dest)
 
 
@@ -120,6 +133,13 @@ def show_plot(titles, imgs, files):
         plt.axis("off")
 
 
+def show_plot_traces(titles, imgs, scales):
+    fig, ax = plt.subplots(1, 2)
+    for title, img, axis, sca in zip(titles, imgs, ax, scales):
+        axis.set_title(title + " (μm)")
+        axis.imshow(img, extent=(0, sca, 0, sca))
+
+
 def plot_segment_object(Input_folder, titles, datatype, RTs_references=""):
     # Figure size in inches optional
     rcParams["figure.figsize"] = 15, 10
@@ -145,39 +165,41 @@ def plot_segment_object(Input_folder, titles, datatype, RTs_references=""):
             )
             imgs = [mpimg.imread(files[0])[1500:3600, 500:4500]]
 
+        print(f"$ Will plot: {files}")
+
+        # Plots images
+        show_plot(titles, imgs, files)
+
     elif datatype == "TRACES":
         # Create list of images
+        tracing_folder = os.path.join(Input_folder, "tracing")
         files = glob.glob(
-            Input_folder
-            + "tracing"
-            + os.sep
-            + "*"
-            + "KDtree"
-            + "*"
-            + "_XYZ"
-            + "*"
-            + ".png"
+            tracing_folder + os.sep + "*" + "KDtree" + "*" + "_XYZ" + "*" + ".png"
         )
         imgs = [mpimg.imread(x) for x in files]
         # crops png to get full and zoomed images
-        imgs = [x[65:1840, 170:1940] for x in imgs]
-        imgs.append(imgs[0][840:1020, 520:700])
+        # this img represent 200*200 micron-meters
+        imgs = [x[70:1840, 170:1940] for x in imgs]
+        scales = [200]
+        imgs.append(imgs[0][1470:1770, 1390:1690])
+        scales.append(200 * (1770 - 1470) / (1840 - 70))
 
-    print(f"$ Will plot: {files}")
+        print(f"$ Will plot: {files}")
 
-    # Plots images
-    show_plot(titles, imgs, files)
+        # Plots images
+        show_plot_traces(titles, imgs, scales)
 
 
 def plot_matrix(input_folder, data_type="proximity"):
     # figure size in inches optional
     rcParams["figure.figsize"] = 15, 15
 
+    tracing_folder = os.path.join(input_folder, "tracing")
     # Create list of images
     if data_type == "PWD":
-        files = glob.glob(input_folder + "tracing" + os.sep + "*_PWDmatrixKDE.png")
+        files = glob.glob(tracing_folder + os.sep + "*_PWDmatrixKDE.png")
     elif data_type == "proximity":
-        files = glob.glob(input_folder + "tracing" + os.sep + "*HiMmatrix.png")
+        files = glob.glob(tracing_folder + os.sep + "*HiMmatrix.png")
     elif data_type.split(",")[0] == "3D_alignments":
         files = glob.glob(
             input_folder
