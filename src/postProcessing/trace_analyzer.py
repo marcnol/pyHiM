bed2ebd60d0e6db96a5745f753602c4fd6d8b399
marcnol <marcnol@gmail.com>
2025-03-24 11:45:46 +0100

    implemented plotting of barcode efficiencies - removed plotting of XYZ distributions - now XYZ trajectory plotting is optional

--- src/postProcessing/trace_analyzer.py
+++ src/postProcessing/trace_analyzer.py
@@ -5,33 +5,59 @@ Created on Fri Jun 17 16:07:05 2022
 
 @author: marcnol
 
-This script will load a trace file and analyze a number of properties such as:
-    - number of barcodes detected per trace
-    - number of duplicated barcodes
-    - trace Rg
+## Description
+A Python script for analyzing chromatin trace files. The script loads trace files and generates statistical analysis and visualizations of various properties:
+- Number of barcodes detected per trace
+- Number of duplicated barcodes
+- Distribution of localizations in X, Y, Z coordinates
+- Distances between consecutive neighboring barcodes
+- Visual representations of traces
 
-$ trace_analyzer.py
+## Dependencies
+- argparse
+- collections
+- select
+- sys
+- matplotlib
+- numpy
+- matrixOperations.chromatin_trace_table (ChromatinTraceTable class)
 
-output:
+## Arguments
 
-trace_stats.csv
+| Flag | Long Form | Description | Default |
+|------|-----------|-------------|---------|
+| `-F` | `--rootFolder` | Folder with images | Current directory (`.`) |
+| | `--input` | Name of input trace file | None |
+| | `--pipe` | Inputs trace file list from stdin | False |
 
-trace_ID, number of barcodes, number of duplications, Rg,
+## Usage
 
+```bash
+# Analyze a single trace file
+./trace_analyzer.py --input path/to/your/trace_file.ecsv
+
+# Analyze multiple trace files using pipe
+find /path/to/traces -name "*.ecsv" | ./trace_analyzer.py --pipe
+
+# Specify a different root folder
+./trace_analyzer.py --rootFolder /custom/data/folder --input trace_file.ecsv
+```
 """
 
 # =============================================================================
 # IMPORTS
-# =============================================================================q
+# =============================================================================
 
 import argparse
 import collections
 import select
 import sys
+from collections import defaultdict
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.gridspec import GridSpec
 
 from matrixOperations.chromatin_trace_table import ChromatinTraceTable
 
@@ -41,13 +67,19 @@ matplotlib.rc("font", **font)
 
 # =============================================================================
 # FUNCTIONS
-# =============================================================================q
+# =============================================================================
 
 
 def parseArguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-F", "--rootFolder", help="Folder with images")
+    parser.add_argument("-F", "--rootFolder", default=".", help="Folder with images")
     parser.add_argument("--input", help="Name of input trace file.")
+    parser.add_argument(
+        "--plotXYZ",
+        default=False,
+        help="Plots XYZ traces for all ROIs",
+        action="store_true",
+    )
     parser.add_argument(
         "--pipe", help="inputs Trace file list from stdin (pipe)", action="store_true"
     )
@@ -55,15 +87,9 @@ def parseArguments():
     p = {}
 
     args = parser.parse_args()
-    if args.rootFolder:
-        p["rootFolder"] = args.rootFolder
-    else:
-        p["rootFolder"] = "."
-
-    if args.input:
-        p["input"] = args.input
-    else:
-        p["input"] = None
+    p["input"] = args.input
+    p["rootFolder"] = args.rootFolder
+    p["plotXYZ"] = args.plotXYZ
 
     p["trace_files"] = []
     if args.pipe:
@@ -84,47 +110,6 @@ def parseArguments():
         p["trace_files"] = [p["input"]]
 
     return p
-
-
-def get_xyz_statistics(trace, output_filename="test_coor.png"):
-    """
-    Function that calculates the
-        - distribution of localizations in x y z
-
-    Parameters
-    ----------
-    trace : TYPE
-        Trace table in ASTROPY Table format.
-    output_filename : TYPE, optional
-        Output figure in PNG. The default is 'test.png'.
-
-    Returns
-    -------
-    None.
-
-    """
-    coords = ["x", "y", "z"]
-
-    fig = plt.figure(constrained_layout=True)
-    im_size, number_plots = 10, 3
-    fig.set_size_inches((im_size * number_plots, im_size))
-    gs = fig.add_gridspec(1, number_plots)
-    axes = [fig.add_subplot(gs[0, i]) for i in range(number_plots)]
-
-    for axis, coor in zip(axes, coords):
-        print(f"$ processing coordinate: {coor}")
-        coordinates = trace[coor].data
-        axis.hist(coordinates, alpha=0.3, bins=20)
-        axis.set_xlabel(coor)
-        axis.set_ylabel("counts")
-        axis.set_title(
-            "n = "
-            + str(len(coordinates))
-            + " | median = "
-            + str(np.median(coordinates))
-        )
-
-    plt.savefig(output_filename)
 
 
 def get_barcode_statistics(trace, output_filename="test_barcodes.png"):
@@ -271,43 +256,69 @@ def plot_neighbor_distances(trace, output_filename="neighbor_distances.png"):
     return mean_dx, mean_dy, mean_dz, std_dx, std_dy, std_dz
 
 
-'''
-def analyze_trace(trace, trace_file):
-    """
-    Launcher function that will perform different kinds of trace analyses
-
-    Parameters
-    ----------
-    trace : ChromatinTraceTable Class
-        Trace table, instance of the ChromatinTraceTable Class.
-    trace_file : string
-        file name of trace table in ecsv format.
-
-    Returns
-    -------
-    None.
-
-    """
+def barcode_detection_efficiency(trace, output_prefix="barcode_detection_efficiency"):
 
     trace_table = trace.data
+    trace_groups = trace_table.group_by("Trace_ID").groups
+    n_traces = len(trace_groups)
+    bootstrap_iterations = 1000
 
-    print(f"$ Number of lines in trace: {len(trace_table)}")
+    print(f"$ Calculating overall barcode detection across {n_traces} traces...")
 
-    output_filename = [trace_file.split(".")[0], "_xyz_statistics", ".png"]
-    get_xyz_statistics(trace_table, "".join(output_filename))
+    barcode_presence = defaultdict(list)
+    all_barcodes = set(trace_table["Barcode #"])
 
-    output_filename = [trace_file.split(".")[0], "_trace_statistics", ".png"]
-    get_barcode_statistics(trace_table, "".join(output_filename))
+    # Track barcode presence per trace
+    for sub_trace in trace_groups:
+        present = set(sub_trace["Barcode #"])
+        for barcode in all_barcodes:
+            barcode_presence[barcode].append(1 if barcode in present else 0)
 
-    # plots statistics of barcodes and saves in file
-    collective_barcode_stats = trace.barcode_statistics(trace_table)
-    trace.plots_barcode_statistics(
-        collective_barcode_stats, file_name=trace_file + "_stats", kind="matrix"
-    )
-'''
+    # Bootstrap detection frequencies
+    detection_stats = {}
+    for barcode, detections in barcode_presence.items():
+        detections = np.array(detections)
+        means = [
+            np.mean(np.random.choice(detections, size=n_traces, replace=True))
+            for _ in range(bootstrap_iterations)
+        ]
+        detection_stats[barcode] = {
+            "mean": np.mean(detections),
+            "ci_lower": np.percentile(means, 2.5),
+            "ci_upper": np.percentile(means, 97.5),
+            "distribution": detections,
+        }
+
+    # Plotting
+    sorted_barcodes = sorted(detection_stats.keys())
+    num_barcodes = len(sorted_barcodes)
+    max_per_row = 25
+    n_rows = (num_barcodes - 1) // max_per_row + 1
+
+    fig_height = 5 * n_rows
+    fig = plt.figure(figsize=(20, fig_height))
+    gs = GridSpec(n_rows, 1, figure=fig)
+
+    for row in range(n_rows):
+        ax = fig.add_subplot(gs[row, 0])
+        start = row * max_per_row
+        end = min(start + max_per_row, num_barcodes)
+        barcodes_row = sorted_barcodes[start:end]
+        data = [detection_stats[bc]["distribution"] for bc in barcodes_row]
+
+        ax.violinplot(data, showmedians=True)
+        ax.set_xticks(np.arange(1, len(barcodes_row) + 1))
+        ax.set_xticklabels(barcodes_row)
+        ax.set_ylabel("Detection frequency")
+        ax.set_ylim(0, 1)
+        ax.set_title(f"Barcodes {start + 1} to {end}")
+
+    fig.tight_layout()
+    fig.savefig(f"{output_prefix}.png")
+    print(f"$ Saved overall barcode detection efficiency plot to: {output_prefix}.png")
 
 
-def analyze_trace(trace, trace_file):
+def analyze_trace(trace, trace_file, plotXYZ=False):
     """
     Launcher function that will perform different kinds of trace analyses.
 
@@ -325,13 +336,16 @@ def analyze_trace(trace, trace_file):
 
     trace_table = trace.data
 
-    print(f"$ Number of lines in trace: {len(trace_table)}")
+    print(f"$ Number of spots in trace file: {len(trace_table)}")
 
-    output_filename = [trace_file.split(".")[0], "_xyz_statistics", ".png"]
-    get_xyz_statistics(trace_table, "".join(output_filename))
-
+    # Calculate trace statistics
     output_filename = [trace_file.split(".")[0], "_trace_statistics", ".png"]
     get_barcode_statistics(trace_table, "".join(output_filename))
+
+    # Plot barcode detection per ROI with bootstrapped errors
+    barcode_detection_efficiency(
+        trace, output_prefix=trace_file.split(".")[0] + "_barcode_detection"
+    )
 
     # Compute and plot neighbor distances
     output_filename = [trace_file.split(".")[0], "_neighbor_distances", ".png"]
@@ -342,14 +356,14 @@ def analyze_trace(trace, trace_file):
         f"$ Mean distances between neighboring barcodes: X={mean_dx:.3f}, Y={mean_dy:.3f}, Z={mean_dz:.3f}"
     )
 
-    # Plots statistics of barcodes and saves in file
+    # Plots how often barcodes are repeated in a single trace
     collective_barcode_stats = trace.barcode_statistics(trace_table)
     trace.plots_barcode_statistics(
         collective_barcode_stats, file_name=trace_file + "_stats", kind="matrix"
     )
 
 
-def process_traces(trace_files=list()):
+def process_traces(p):
     """
     Processes list of trace files and sends each to get analyzed individually
 
@@ -365,6 +379,7 @@ def process_traces(trace_files=list()):
     None.
 
     """
+    trace_files = p["trace_files"]
 
     if len(trace_files) > 0:
         print(
@@ -381,14 +396,15 @@ def process_traces(trace_files=list()):
             # reads new trace
             trace.load(trace_file)
 
-            print(f"> Plotting traces for {trace_file}")
-            trace.plots_traces(
-                [trace_file.split(".")[0], "_traces_XYZ", ".png"],
-                pixel_size=[0.1, 0.1, 0.25],
-            )
+            if p["plotXYZ"]:
+                print(f"> Plotting traces for {trace_file}")
+                trace.plots_traces(
+                    [trace_file.split(".")[0], "_traces_XYZ", ".png"],
+                    pixel_size=[0.1, 0.1, 0.25],
+                )
 
             print(f"> Analyzing traces for {trace_file}")
-            analyze_trace(trace, trace_file)
+            analyze_trace(trace, trace_file, plotXYZ=p["plotXYZ"])
 
     else:
         print(
@@ -406,7 +422,7 @@ def main():
     p = parseArguments()
 
     # [loops over lists of datafolders]
-    process_traces(trace_files=p["trace_files"])
+    process_traces(p)
 
     print("Finished execution")
 
