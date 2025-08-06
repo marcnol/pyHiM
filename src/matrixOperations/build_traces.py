b3de61c52aa5cc28b4c76209f219b1ee54517c14
Xavier DEVOS <xavierdevos@outlook.fr>
2025-08-06 09:07:14 +0200

    fix(mask): avoid relabelling masks when we don't need to expand_labels - in the case of very close masks, relabelling will create bad giant masks

--- src/matrixOperations/build_traces.py
+++ src/matrixOperations/build_traces.py
@@ -379,15 +379,18 @@ class BuildTraces:
                 segmented_masks = read_array(full_filename_masks)
                 print_log(f"$ loaded mask file: {full_filename_masks}")
 
-                # expands mask without overlap by a maximum of 'distance' pixels
-                self.masks = expand_labels(
-                    segmented_masks, distance=matrix_params.mask_expansion
-                )
+                if matrix_params.mask_expansion:
+                    # expands mask without overlap by a maximum of 'distance' pixels
+                    self.masks = expand_labels(
+                        segmented_masks, distance=matrix_params.mask_expansion
+                    )
 
-                # Generate new labels for each connected component to ensure each label is assigned to 1 mask only.
-                self.masks = label(
-                    self.masks > 0, connectivity=1
-                )  # Relabel masks uniquely
+                    # Generate new labels for each connected component to ensure each label is assigned to 1 mask only.
+                    self.masks = label(
+                        self.masks > 0, connectivity=1
+                    )  # Relabel masks uniquely
+                else:
+                    self.masks = segmented_masks
 
                 # initializes masks
                 self.initializes_masks(self.masks)
