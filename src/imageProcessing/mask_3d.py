591e73c9ca19ccf1d22e4f481e70c51d78959f24
amelzd <azidoum@protonmail.com>
2026-05-28 10:44:54 +0200

    Update mask_3d.py

--- src/imageProcessing/mask_3d.py
+++ src/imageProcessing/mask_3d.py
@@ -4,10 +4,15 @@
 
 from core.parameters import SegmentationParams
 from imageProcessing.makeProjections import Feature
-
+# from imageProcessing.alignImages3D import Class3D
+# import apply DF function 
 
 class Mask3D(Feature):
     def __init__(self, params: SegmentationParams):
         super().__init__(params)
         self.out_folder = self.params.mask_3d_folder
         self.name = "Mask3D"
+        #  if DF exists :
+            #    warped_new_np = applyDF(new_image_np,displacement_fields_np, fixed_image_np.shape)
+            #    warped_new_sitk = to_sitk(warped_new_np, ref_img=fixed_image)
+            #    write_image(warped_new_sitk, "name_image_corrected.tif")
